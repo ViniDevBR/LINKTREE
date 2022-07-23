12ffe0611d7330379c8f44e9ba27eda055834dfe
Rogerio de Oliveira <vinigtr386@hotmail.com>
2022-07-23 14:28:04 -0300

    initial commit

---
+++ ChangeColors.js
@@ -0,0 +1,46 @@
+const bodyBackGround = document.querySelector('body')
+const colors = document.querySelector('input[type=color]')
+const menuOpen = document.querySelector('.themes')
+const themesOpen = document.querySelector('.button-themes')
+const hamburguerOpen = document.querySelector('.hamburguer')
+const darkTheme = document.querySelector('#dark')
+const blueDarkTheme = document.querySelector('#blueDark')
+const pinkDarkTheme = document.querySelector('#pinkDark')
+const lightTheme = document.querySelector('#light')
+
+hamburguerOpen.addEventListener('click', () =>{
+  hamburguerOpen.classList.toggle('active')
+  menuOpen.classList.toggle('on')
+})
+
+darkTheme.addEventListener('click', () =>{
+  bodyBackGround.classList.add('dark')
+  bodyBackGround.classList.remove('blueDark', 'pinkDark')
+})
+
+blueDarkTheme.addEventListener('click', () =>{
+  bodyBackGround.classList.add('blueDark')
+  bodyBackGround.classList.remove('dark', 'pinkDark')
+})
+
+pinkDarkTheme.addEventListener('click', () =>{
+  bodyBackGround.classList.add('pinkDark')
+  bodyBackGround.classList.remove('blueDark', 'dark')
+})
+
+lightTheme.addEventListener('click', () =>{
+  bodyBackGround.classList.remove('dark', 'blueDark', 'pinkDark')
+})
+
+themesOpen.addEventListener('click', () =>{
+  menuOpen.classList.toggle('on')
+})
+console.log('Links Vinicius - version 1.0 | Me contrata')
+
+
+
+
+
+
+
+
